a9997e15407af8011af76b2c8332a902bf0bff83
Jonathan Poncelet <j.poncelet@faceit.com>
2021-08-15 17:37:43 +0100

    Switched to CMake with appropriate dependencies

--- main.cpp
+++ main.cpp
@@ -2,10 +2,13 @@
 
 #include <QDebug>
 #include <QApplication>
+#include <QCoreApplication>
 
 int main(int argc, char *argv[])
 {
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication a(argc, argv);
+
     MainWindow w;
 
     w.showSplash();
